c2ef318269b54bb4df9111e1882a9ebb8cdeb695
danielbulgakov <bulgach09@gmail.com>
2022-11-10 21:45:48 +0300

    region params added

--- OwnSolution/RegionParams.cs
+++ OwnSolution/RegionParams.cs
@@ -1,13 +1,48 @@
 
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
 namespace ImageTextureLab.OwnSolution
 {
     
     public class RegionParams
     {
+        // not tested yet.
         public static int RegionSquare(Region r)
         {
-            return 0;
+            return r.PixelList.Count;
         }
+
+        // not tested yet.
+        public static int RegionPerimeter(Region r)
+        {
+            throw new NotImplementedException();
+            var list = r.PixelList;
+            list.Sort();
+
+            bool isBorder = false;
+            int borderCount = 0;
+            
+
+            return -1;
+        }
+
+        public static int RegionDiscreteMomentum(Region r, int x, int y, Bitmap image)
+        {
+            int momentum = 0;
+
+            foreach (var pix in r.PixelList)
+            {
+                momentum += (int)Math.Pow(pix.Item1, x) *
+                            (int)Math.Pow(pix.Item2, y) *
+                            Tools.ImageTools.GetBrightness(image.GetPixel(x ,y));
+
+            }
+
+            return momentum;
+        }
+        
         
     }
 }
